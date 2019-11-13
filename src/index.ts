f60b64f2e10c5a7eedc98d775ea06c9ee1880621
Oleg Koval <orthodoz@gmail.com>
2019-11-13 22:02:35 +0100

    chore: test again

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,20 @@ import { handleBranchFlag, handleDryRunFlag } from './optionsHandlers';
 import { Commands, reportResults, runTask } from './tasks';
 
 const main = async (): Promise<void> => {
+  // eslint-disable-next-line no-console
+  console.log(':::: START');
+
   await runTask(Commands.CreateGitIgnoreBackup);
 
   const result = await semanticRelease({
-    branch: 'development',
     ci: false,
     ...defaultConfiguration,
     ...handleBranchFlag(),
     ...handleDryRunFlag(),
   });
+
+  // eslint-disable-next-line no-console
+  console.log('TCL: result', result);
 
   await runTask(Commands.RemoveNpmrc);
   await reportResults(result);
