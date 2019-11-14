42ad9eb3334498f03e4691408529ba2b50b9090b
Ed <oleg@ridedott.com>
2019-11-14 14:03:37 +0100

    Update src/config/transform.ts
    Co-Authored-By: Natan Sągol <m@merlinnot.com>

--- src/config/transform.ts
+++ src/config/transform.ts
@@ -3,7 +3,7 @@
 /**
  * This transform function is a modified copy of
  * Angular preset for https://github.com/semantic-release/commit-analyzer
- * to support release notes & changelog entries generation for all types of commit messages:
+ * to support release notes and changelog entries generation for all types of commit messages:
  * https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-angular
  * Ignored in .eslintignore to speedup development of action. Same reasoning for usage of @ts-ignore annotations
  */
