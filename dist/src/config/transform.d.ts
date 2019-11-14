25f92e3c5ac99d76cb0017ca5ead3229fdb8696c
Oleg Koval <orthodoz@gmail.com>
2019-11-14 10:37:52 +0100

    style: fix typo

--- dist/src/config/transform.d.ts
+++ dist/src/config/transform.d.ts
@@ -1,5 +1,5 @@
 /**
- * This transform functions is a modified copy of
+ * This transform function is a modified copy of
  * angular preset for https://github.com/semantic-release/commit-analyzer
  * to support release notes & changelog entries generation for all types of commit messages:
  * https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-angular
