9000bb5869deee7c327ea0880ab813cdf4ffcd50
Oleg Koval <orthodoz@gmail.com>
2019-11-14 10:58:18 +0100

    style: fix comments

--- src/config/plugins.ts
+++ src/config/plugins.ts
@@ -16,9 +16,8 @@ export const plugins = [
     '@semantic-release/git',
     {
       assets: ['dist', 'package.json', 'package-lock.json', 'CHANGELOG.md'],
-      /* eslint-disable no-template-curly-in-string */
+      // eslint-disable-next-line no-template-curly-in-string
       message: 'chore(release): ${nextRelease.version} [skip ci]',
-      /* eslint-enable no-template-curly-in-string */
     },
   ],
   [
